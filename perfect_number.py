517d0b30ab3f3ebdc2ec8eab21a206ee76ae62c1
eleonorakuteva <eleonorakuteva@gmail.com>
2025-06-05 19:47:39 +0300

    Create perfect_number.py

---
+++ perfect_number.py
@@ -0,0 +1,36 @@
+# ====== git hub - PERFECT NUMBER ======
+
+def find_perfect_number(given_number:int):
+    """
+    A perfect number is a positive integer that is equal to the sum of its proper positive divisors.
+    That is the sum of its positive divisors, excluding the number itself (also known as its aliquot sum).
+
+    :param given_number: receives an integer number
+    :return: the following messages as string:
+        "We have a perfect number!" - if the number is perfect.
+        "It's not so perfect." - if the number is NOT perfect.
+    """
+
+    list_of_proper_positive_divisors:list = []
+    for previous_num in range(1, (given_number//2) +1):
+        if given_number % previous_num == 0:
+            list_of_proper_positive_divisors.append(previous_num)
+    # print(list_of_proper_positive_divisors)
+
+    if sum(list_of_proper_positive_divisors) == given_number:
+        returned_result = "We have a perfect number!"
+    else:
+        returned_result = "It's not so perfect."
+
+    return returned_result
+
+
+try:
+    user_num:int = int(input("Enter a positive integer: "))
+    if user_num <= 0:
+        print("Perfect numbers must be positive integers greater than zero.")
+    else:
+        result = find_perfect_number(user_num)
+        print(result)
+except ValueError:
+    print("Invalid input. Please enter a whole, positive number.")
