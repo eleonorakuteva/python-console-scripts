d48ba499df2db3989fa5a52bde6c6b2676a8dd22
eleonorakuteva <eleonorakuteva@gmail.com>
2025-07-01 19:10:47 +0300

    Update library_system.py

--- library_system.py
+++ library_system.py
@@ -15,7 +15,26 @@ class Library:
 
 
 class LibraryUser:
+
+    _user_count = 0
+
+    @classmethod
+    def get_user_count(cls):
+        return LibraryUser._user_count
+
     def __init__(self, first_name:str, last_name:str, borrowed_books = None):
         self.first_name = first_name
         self.last_name = last_name
         self.borrowed_books = borrowed_books
+        LibraryUser._user_count += 1
+
+    @ property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def borrow_book(self, book, library):
+        pass
+
+    def return_book(self, library):
+        pass
+
