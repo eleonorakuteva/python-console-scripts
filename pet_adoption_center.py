72021d0ea74ccf35c07f81cac7511a1229844ef1
eleonorakuteva <eleonorakuteva@gmail.com>
2025-07-02 13:36:23 +0300

    Update pet_adoption_center.py

--- pet_adoption_center.py
+++ pet_adoption_center.py
@@ -3,9 +3,9 @@ Project name: Pet Adoption Center Management System
 
 """
 class AdoptionCenter:
-    def __init__(self, name:str):
+    def __init__(self, name:str, list_of_pets = None):
         self.name = name
-        self.list_of_pets = None
+        self.list_of_pets = list_of_pets if list_of_pets is not None else []
 
     def __repr__(self):
         return (f"{self.name} is a adoption center, where you can adopt your future pet!\n"
@@ -25,9 +25,9 @@ class Pet:
         self.is_adopted = False
 
 class Adopter:
-    def __init__(self, name:str):
+    def __init__(self, name:str, adopted_pets_by_user = None):
         self.name = name
-        self.list_of_pets = None
+        self.adopted_pets_by_user = adopted_pets_by_user if adopted_pets_by_user is not None else []
 
     def adopt_pet(self, pet:str, center:str):
         pass
@@ -35,13 +35,24 @@ class Adopter:
     def return_pet(self, pet:str, center:str):
         pass
 
+    def user_adopted_pets(self):
+        pass
+
+    def __repr__(self):
+        return (f"Adopter name: {self.name}.\n"
+                f"Adopted pets: {", ".join(self.adopted_pets_by_user)}.")
+
 class Worker(Adopter):
-    def __init__(self, name:str, worker_id:str):
-        super().__init__(name)
-        self.worker_id = worker_id
+    def __init__(self, name:str, worker_id:str, adopted_pets_by_user = None):
+        super().__init__(name, adopted_pets_by_user)
+        self._worker_id = worker_id
 
     def add_pet(self, pet, center):
         pass
 
     def remove_pet(self, pet, center):
         pass
+
+    def __repr__(self):
+        return (f"Worker name: {self.name}.\n"
+                f"Worker Id: {self._worker_id}.")
