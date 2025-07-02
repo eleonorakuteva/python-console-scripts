7fccae7997964594e7093e8cd21c6b7c25c38730
eleonorakuteva <eleonorakuteva@gmail.com>
2025-07-02 13:20:38 +0300

    Update pet_adoption_center.py

--- pet_adoption_center.py
+++ pet_adoption_center.py
@@ -17,3 +17,25 @@ class Pet:
         self.species = species
         self.age = age
         self.is_adopted = False
+
+class Adopter:
+    def __init__(self, name:str):
+        self.name = name
+        self.list_of_pets = None
+
+    def adopt_pet(self, pet:str, center:str):
+        pass
+
+    def return_pet(self, pet:str, center:str):
+        pass
+
+class Worker(Adopter):
+    def __init__(self, name:str, worker_id:str):
+        super().__init__(name)
+        self.worker_id = worker_id
+
+    def add_pet(self, pet, center):
+        pass
+
+    def remove_pet(self, pet, center):
+        pass
