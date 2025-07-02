c1271fb6a1002472563b5ad8c3edccadd5b2f802
eleonorakuteva <eleonorakuteva@gmail.com>
2025-07-02 13:38:09 +0300

    Update pet_adoption_center.py

--- pet_adoption_center.py
+++ pet_adoption_center.py
@@ -1,6 +1,9 @@
 """
 Project name: Pet Adoption Center Management System
 
+System for a pet adoption center that manages pets, adopters, and staff members.
+The center keeps track of available pets, and adopters can adopt or return pets.
+Staff (workers) can add or remove pets from the center.
 """
 class AdoptionCenter:
     def __init__(self, name:str, list_of_pets = None):
