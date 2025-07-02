c09523af62cdf084b61f0d8ab67cdedb8148cddc
eleonorakuteva <eleonorakuteva@gmail.com>
2025-07-02 14:15:32 +0300

    Update pet_adoption_center.py

--- pet_adoption_center.py
+++ pet_adoption_center.py
@@ -5,6 +5,8 @@ System for a pet adoption center that manages pets, adopters, and staff members.
 The center keeps track of available pets, and adopters can adopt or return pets.
 Staff (workers) can add or remove pets from the center.
 """
+
+
 class AdoptionCenter:
     def __init__(self, name:str, list_of_pets = None):
         self.name = name
@@ -15,10 +17,9 @@ class AdoptionCenter:
                 f"In the center there are registered animals: {", ".join(self.list_of_pets)} ")
 
     def list_available_pets(self):
-        pass
+        return (f"Available pets you can adopt:\n"
+                f"{', '.join(self.list_of_pets)}")
 
-    def list_all_pets(self):
-        pass
 
 class Pet:
     def __init__(self, name:str, species:str, age:int):
@@ -46,16 +47,44 @@ class Adopter:
                 f"Adopted pets: {", ".join(self.adopted_pets_by_user)}.")
 
 class Worker(Adopter):
+
     def __init__(self, name:str, worker_id:str, adopted_pets_by_user = None):
         super().__init__(name, adopted_pets_by_user)
         self._worker_id = worker_id
 
-    def add_pet(self, pet, center):
-        pass
+    def is_id_valid(self):
+        return len(self._worker_id) == 8 and self._worker_id.isdigit()
 
-    def remove_pet(self, pet, center):
-        pass
+    def add_pet(self, pet, adoption_center):
+        if self.is_id_valid():
+            adoption_center.list_of_pets.append([pet.species, pet.name, pet.age])
+
+    def remove_pet(self, pet, adoption_center):
+        if self.is_id_valid():
+            adoption_center.list_of_pets.remove(pet)
 
     def __repr__(self):
         return (f"Worker name: {self.name}.\n"
-                f"Worker Id: {self._worker_id}.")
+                f"Worker Id: {self._worker_id}, valid = {self.is_id_valid()}.")
+
+
+if __name__ == "__main__":
+    center = AdoptionCenter("SunnyPaws")
+
+    pet1 = Pet("Bella", "Dog", 3)
+    pet2 = Pet("Luna", "Cat", 1)
+
+    worker = Worker("James", "10001023")
+    adopter = Adopter("Maria", "10001024")
+    print(worker)
+
+    worker.add_pet(pet1, center)
+    worker.add_pet(pet2, center)
+
+    center.list_available_pets()
+
+    adopter.adopt_pet(pet1, center)
+    center.list_available_pets()
+
+    # adopter.return_pet(pet1, center)
+    # center.list_available_pets()
